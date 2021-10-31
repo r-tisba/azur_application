2cd9838cfa21f8515b8c45c140d8f178205a9d8d
Raphaël Tisba <raphaeltisba@yahoo.fr>
2021-10-31 19:24:05 +0100

    Ajout du dashboard, de l'onglet utilisateurs et son RAUD

---
+++ azur_application/gestion.cs
@@ -0,0 +1,403 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CustomWindowsForm;
+using System.Threading;
+using MySql.Data.MySqlClient;
+using Syncfusion.Windows.Forms.Tools;
+
+namespace azur_application
+{
+    public partial class gestion : Form
+    {
+        MySqlConnection conn = new MySqlConnection("database=gestion; server=localhost; user id=root; pwd=");
+
+        private Button currentButton;
+        private Random random;
+        private int tempIndex;
+        private Form activeForm;
+
+
+        public gestion()
+        {
+            InitializeComponent();
+            random = new Random();
+            btnCloseChild.Visible = false;
+        }
+
+        bool isTopPanelDragged = false;
+        bool isLeftPanelDragged = false;
+        bool isRightPanelDragged = false;
+        bool isBottomPanelDragged = false;
+        bool isTopBorderPanelDragged = false;
+        bool isWindowMaximized = false;
+        Point offset;
+        Size _normalWindowSize;
+        Point _normalWindowLocation = Point.Empty;
+
+        // ------------------------------------ DEPLACEMENT & RESIZE DE LA FENETRE ------------------------------------
+        private void TopBorderPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Y < this.Location.Y)
+            {
+                if (isTopBorderPanelDragged)
+                {
+                    if (this.Height < 50)
+                    {
+                        this.Height = 50;
+                        isTopBorderPanelDragged = false;
+                    }
+                    else
+                    {
+                        this.Location = new Point(this.Location.X, this.Location.Y + e.Y);
+                        this.Height = this.Height - e.Y;
+                    }
+                }
+            }
+        }
+        private void TopBorderPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            isTopBorderPanelDragged = false;
+        }
+        private void TopBorderPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isTopBorderPanelDragged = true;
+            }
+            else
+            {
+                isTopBorderPanelDragged = false;
+            }
+        }
+
+        private void TopPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isTopPanelDragged)
+            {
+                Point newPoint = TopPanel.PointToScreen(new Point(e.X, e.Y));
+                newPoint.Offset(offset);
+                this.Location = newPoint;
+
+                if (this.Location.X > 2 || this.Location.Y > 2)
+                {
+                    if (this.WindowState == FormWindowState.Maximized)
+                    {
+                        this.Location = _normalWindowLocation;
+                        this.Size = _normalWindowSize;
+                        toolTip1.SetToolTip(_MaxButton, "Maximize");
+                        _MaxButton.CFormState = MinMaxButton.CustomFormState.Normal;
+                        isWindowMaximized = false;
+                    }
+                }
+            }
+        }
+        private void TopPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            isTopPanelDragged = false;
+            if (this.Location.Y <= 5)
+            {
+                if (!isWindowMaximized)
+                {
+                    _normalWindowSize = this.Size;
+                    _normalWindowLocation = this.Location;
+
+                    Rectangle rect = Screen.PrimaryScreen.WorkingArea;
+                    this.Location = new Point(0, 0);
+                    this.Size = new System.Drawing.Size(rect.Width, rect.Height);
+                    toolTip1.SetToolTip(_MaxButton, "Restore Down");
+                    _MaxButton.CFormState = MinMaxButton.CustomFormState.Maximize;
+                    isWindowMaximized = true;
+                }
+            }
+        }
+        private void TopPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isTopPanelDragged = true;
+                Point pointStartPosition = this.PointToScreen(new Point(e.X, e.Y));
+                offset = new Point();
+                offset.X = this.Location.X - pointStartPosition.X;
+                offset.Y = this.Location.Y - pointStartPosition.Y;
+            }
+            else
+            {
+                isTopPanelDragged = false;
+            }
+            if (e.Clicks == 2)
+            {
+                isTopPanelDragged = false;
+                _MaxButton_Click(sender, e);
+            }
+        }
+
+        private void LeftPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.X < this.Location.X)
+            {
+                if (isLeftPanelDragged)
+                {
+                    if (this.Width < 100)
+                    {
+                        this.Width = 100;
+                        isLeftPanelDragged = false;
+                    }
+                    else
+                    {
+                        this.Location = new Point(this.Location.X + e.X, this.Location.Y);
+                        this.Width = this.Width - e.X;
+                    }
+                }
+            }
+        }
+        private void LeftPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            isLeftPanelDragged = false;
+        }
+        private void LeftPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (this.Location.X <= 0 || e.X < 0)
+            {
+                isLeftPanelDragged = false;
+                this.Location = new Point(10, this.Location.Y);
+            }
+            else
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    isLeftPanelDragged = true;
+                }
+                else
+                {
+                    isLeftPanelDragged = false;
+                }
+            }
+        }
+
+        private void RightPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isRightPanelDragged)
+            {
+                if (this.Width < 100)
+                {
+                    this.Width = 100;
+                    isRightPanelDragged = false;
+                }
+                else
+                {
+                    this.Width = this.Width + e.X;
+                }
+            }
+        }
+        private void RightPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            isRightPanelDragged = false;
+        }
+        private void RightPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isRightPanelDragged = true;
+            }
+            else
+            {
+                isRightPanelDragged = false;
+            }
+        }
+
+        private void BottomPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isBottomPanelDragged)
+            {
+                if (this.Height < 50)
+                {
+                    this.Height = 50;
+                    isBottomPanelDragged = false;
+                }
+                else
+                {
+                    this.Height = this.Height + e.Y;
+                }
+            }
+        }
+        private void BottomPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            isBottomPanelDragged = false;
+        }
+        private void BottomPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isBottomPanelDragged = true;
+            }
+            else
+            {
+                isBottomPanelDragged = false;
+            }
+        }
+
+        // ------------------------------------ BOUTONS MIN, MAX, CLOSE ------------------------------------
+        private void _MinButton_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        private void _MaxButton_Click(object sender, EventArgs e)
+        {
+            if (isWindowMaximized)
+            {
+                this.Location = _normalWindowLocation;
+                this.Size = _normalWindowSize;
+                toolTip1.SetToolTip(_MaxButton, "Maximize");
+                _MaxButton.CFormState = MinMaxButton.CustomFormState.Normal;
+                isWindowMaximized = false;
+            }
+            else
+            {
+                _normalWindowSize = this.Size;
+                _normalWindowLocation = this.Location;
+
+                Rectangle rect = Screen.PrimaryScreen.WorkingArea;
+                this.Location = new Point(0, 0);
+                this.Size = new System.Drawing.Size(rect.Width, rect.Height);
+                toolTip1.SetToolTip(_MaxButton, "Restore Down");
+                _MaxButton.CFormState = MinMaxButton.CustomFormState.Maximize;
+                isWindowMaximized = true;
+            }
+        }
+        private void _CloseButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // ------------------------------------ STYLE BOUTONS MENU ------------------------------------
+
+        // Selectionne une couleur random à partir de la liste
+        private Color SelectThemeColor()
+        {
+            int index = random.Next(ThemeColor.ColorList.Count);
+            // Si la couleur à déjà été selectionné, on en pioche une nouvelle
+            while (tempIndex == index)
+            {
+                index = random.Next(ThemeColor.ColorList.Count);
+            }
+            tempIndex = index;
+            string color = ThemeColor.ColorList[index];
+            return ColorTranslator.FromHtml(color);
+        }
+        private void ActivateButton(object btnSender)
+        {
+            if (btnSender != null)
+            {
+                // Modifie l'apparence du bouton cliqué & panel titre
+                if (currentButton != (Button)btnSender)
+                {
+                    DisableButton();
+                    Color color = SelectThemeColor();
+                    currentButton = (Button)btnSender;
+                    currentButton.BackColor = color;
+                    currentButton.ForeColor = Color.White;
+                    currentButton.Font = new System.Drawing.Font("Lato", 12.5F);
+                    // Modifie la couleur du panel titre
+                    panelTitle.BackColor = color;
+
+                    ThemeColor.ActiveColor = color;
+
+                    btnCloseChild.Visible = true;
+                }
+            }
+        }
+        // Etat par défaut du bouton
+        private void DisableButton()
+        {
+            foreach (Control previousBtn in panelMenu.Controls)
+            {
+                if (previousBtn.GetType() == typeof(Button))
+                {
+                    previousBtn.BackColor = Color.FromArgb(52, 58, 64);
+                    previousBtn.ForeColor = Color.Gainsboro;
+                    previousBtn.Font = new System.Drawing.Font("Lato", 10F);
+                }
+            }
+        }
+
+        // ------------------------------------ OUVERTURE DES ONGLETS ------------------------------------
+
+        // Ouverture de la sous-page dans le panel body
+        private void OpenChildForm(Form childForm, object btnSender)
+        {
+            if(activeForm != null)
+            {
+                activeForm.Close();
+            }
+            ActivateButton(btnSender);
+            // Display de l'onglet dans le panel body
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            this.panelBody.Controls.Add(childForm);
+            this.panelBody.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+            // Change le titre par celui de l'onglet
+            labelTitre.Text = childForm.Text;
+            labelTitre.ForeColor = Color.White;
+
+
+        }
+
+        // ------------------------------------ BOUTONS NAVBAR ------------------------------------
+        private void btnProfil_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Onglets.ongletProfil(), sender);
+        }
+
+        private void btnUtilisateurs_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Onglets.ongletUtilisateurs(), sender);
+        }
+
+        private void btnProjets_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Onglets.ongletProjets(), sender);
+        }
+
+        private void btnStatistiques_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Onglets.ongletStatistiques(), sender);
+        }
+
+        private void btnParametres_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Onglets.ongletParametres(), sender);
+        }
+
+        private void btnCloseChild_Click(object sender, EventArgs e)
+        {
+            if (activeForm != null)
+            {
+                activeForm.Close();
+            }
+            Reset();
+
+
+        }
+        // ------------------------------------ RESET ------------------------------------
+        private void Reset()
+        {
+            DisableButton();
+            labelTitre.Text = "ACCUEIL";
+            panelTitle.BackColor = Color.FromArgb(36, 42, 48);
+            panelLogo.BackColor = Color.FromArgb(0, 132, 234);
+            currentButton = null;
+            btnCloseChild.Visible = false;
+        }
+    }
+}
